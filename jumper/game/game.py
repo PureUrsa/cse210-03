b08754127ac9be5ad0d79690070bdf8784e78915
PureUrsa <98349149+PureUrsa@users.noreply.github.com>
2022-02-05 09:55:16 -0700

    Delete jumper/game directory
    We don't want main to be touched until the end, so into the garbage!

--- jumper/game/game.py
+++
@@ -1,9 +0,0 @@
-
-
-class Game:
-    """
-    This class is in charge of the creation of the Jumper game.
-    It will start the game. 
-    """
-    def __init__(self) -> None:
-        pass
--- jumper/game/puzzle.py
+++
@@ -1,9 +0,0 @@
-
-import random
-
-class Puzzle:
-    """
-    This class will generate a secret work from a list. 
-    """
-    def __init__(self) -> None:
-        pass
