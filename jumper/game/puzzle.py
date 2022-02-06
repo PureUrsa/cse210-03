98ac8e56ea805f7c22e9e9a86b596aebd84d608f
PureUrsa <98349149+PureUrsa@users.noreply.github.com>
2022-02-05 18:27:54 -0700

    Update puzzle.py
    A GREAT IDEA!!! TWO LISTS SO THE PLAYER CAN GUESS :D :D :D

--- jumper/game/puzzle.py
+++ jumper/game/puzzle.py
@@ -7,7 +7,7 @@ class Puzzle:
     def __init__(self) -> None:
         pass
     
-    
+    letters = []
     
     words = [plant, beach, dance, house]
     #I think its a good idea to keep this at five letters for simplicity
@@ -16,4 +16,16 @@ class Puzzle:
     
     
     
+    #im pretty sure this is not even vaugly close to how it works but you get the idea :D
+    if random.choice(words) = plant:
+        letters.insert(p, l, a, n, t)
+        
+    elif random.choice(words) = beach:
+        letters.insert(b, e, a, c, h)
+        
+    elif random.choice(words) = dance:
+        letters.insert(d, a, n, c, e)
+    
+    elif random.choice(words) = house:
+        letters.insert(h, o, u, s, e)
     
